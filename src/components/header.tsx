fc5cb50de5268bf825ff16a8cf8621649e7981f9
Ali Muhammad <alimuhammadfc5@gmail.com>
2023-02-16 17:49:30 +0500

    fixing header scroll

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -24,75 +24,88 @@ const Header: FC = () => {
 	});
 
 	return (
-		<header
+		<div
 			className={classNames(
 				scrolled ? "bg-white" : "bg-transparent",
-				"fixed w-full z-50 max-w-screen-2xl px-4 sm:px-6 transition-all duration-300 ease-in-out top-0 left-0 right-0 mx-auto h-20 flex items-center justify-between"
+				"fixed w-full z-50 top-0 left-0 right-0 transition-all duration-300 ease-in-out"
 			)}
 		>
-			<a
-				href="/"
-				className="flex items-center space-x-1 text-xl font-bold"
+			<header
+				className={classNames(
+					"max-w-screen-2xl px-4 sm:px-6 mx-auto h-20 flex items-center justify-between"
+				)}
 			>
-				<img src={logoSvg} className="h-7 w-7" />
-				<span className="text-schema-darkBlue">Apps</span>
-				<span>Market</span>
-			</a>
+				<a
+					href="/"
+					className="flex items-center space-x-1 text-xl font-bold"
+				>
+					<img src={logoSvg} className="h-7 w-7" />
+					<span className="text-schema-darkBlue">Apps</span>
+					<span>Market</span>
+				</a>
 
-			<ul className="hidden lg:flex items-center space-x-3">
-				<li
-					className={classNames(
-						scrolled ? "text-black" : "text-white",
-						"navLinks border-b-4 border-schema-darkBlue"
-					)}
-				>
-					Home
-				</li>
-				<li
-					className={classNames(
-						scrolled ? "text-black" : "text-white",
-						"navLinks"
-					)}
-				>
-					Services
-				</li>
-				<li
-					className={classNames(
-						scrolled ? "text-black" : "text-white",
-						"navLinks"
-					)}
-				>
-					Why Us
-				</li>
-				<li
-					className={classNames(
-						scrolled ? "text-black" : "text-white",
-						"navLinks"
-					)}
-				>
-					Our Goals
-				</li>
-				<li
-					className={classNames(
-						scrolled ? "text-black" : "text-white",
-						"navLinks"
-					)}
-				>
-					Our Portfolio
-				</li>
-				<li
-					className={classNames(
-						scrolled ? "text-black" : "text-white",
-						"navLinks"
-					)}
-				>
-					Our Client
-				</li>
-				<li className="bg-white rounded-full px-5 py-2 hover:bg-gray-300 duration-300 ease-in-out cursor-pointer">
-					Contact Us
-				</li>
-			</ul>
-		</header>
+				<ul className="hidden lg:flex items-center space-x-3">
+					<li
+						className={classNames(
+							scrolled ? "text-black" : "text-white",
+							"navLinks border-b-4 border-schema-darkBlue"
+						)}
+					>
+						Home
+					</li>
+					<li
+						className={classNames(
+							scrolled ? "text-black" : "text-white",
+							"navLinks"
+						)}
+					>
+						Services
+					</li>
+					<li
+						className={classNames(
+							scrolled ? "text-black" : "text-white",
+							"navLinks"
+						)}
+					>
+						Why Us
+					</li>
+					<li
+						className={classNames(
+							scrolled ? "text-black" : "text-white",
+							"navLinks"
+						)}
+					>
+						Our Goals
+					</li>
+					<li
+						className={classNames(
+							scrolled ? "text-black" : "text-white",
+							"navLinks"
+						)}
+					>
+						Our Portfolio
+					</li>
+					<li
+						className={classNames(
+							scrolled ? "text-black" : "text-white",
+							"navLinks"
+						)}
+					>
+						Our Client
+					</li>
+					<li
+						className={classNames(
+							scrolled
+								? "bg-black text-white hover:bg-gray-700 "
+								: "bg-white text-black hover:bg-gray-300 ",
+							"rounded-full px-5 py-2 duration-300 ease-in-out cursor-pointer"
+						)}
+					>
+						Contact Us
+					</li>
+				</ul>
+			</header>
+		</div>
 	);
 };
 
--- src/components/services.tsx
+++ src/components/services.tsx
@@ -6,17 +6,15 @@ const Services: FC = () => {
 			<h2 className="font-medium text-5xl text-center max-w-screen-md">
 				We provide various kind of service for you
 			</h2>
-			<p className="max-w-4xl text-center text-lg mt-3 text-black/60">
+			<p className="max-w-3xl text-center text-lg mt-3 text-black/60">
 				It is a long established fact that a reader will be distracted
 				by the readable content of a page when looking at its layout.
 				The point of using Lorem It is a long established fact that
 			</p>
 
-            <div>
-                <div>
-                    
-                </div>
-            </div>
+			<div>
+				<div></div>
+			</div>
 		</div>
 	);
 };
