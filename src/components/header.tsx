5f5145503536c8e4988aecc623be49976749b92e
Ali Muhammad <alimuhammadfc5@gmail.com>
2023-02-16 17:25:37 +0500

    change header color on scroll

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,35 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import logoSvg from "/logo.svg";
+import classNames from "classnames";
 
 //TODO:highlight what area we are in
 
 const Header: FC = () => {
+	const [scrolled, setScrolled] = useState<boolean>(false);
+
+	const setScrollToTrue = () => {
+		if (window.scrollY > 15) {
+			setScrolled(true);
+		} else {
+			setScrolled(false);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", setScrollToTrue);
+
+		return () => {
+			window.removeEventListener("scroll", setScrollToTrue);
+		};
+	});
+
 	return (
-		<header className="fixed w-full z-50 max-w-screen-2xl px-4 sm:px-6 top-0 left-0 right-0 mx-auto h-20 flex items-center justify-between">
+		<header
+			className={classNames(
+				scrolled ? "bg-white" : "bg-transparent",
+				"fixed w-full z-50 max-w-screen-2xl px-4 sm:px-6 transition-all duration-300 ease-in-out top-0 left-0 right-0 mx-auto h-20 flex items-center justify-between"
+			)}
+		>
 			<a
 				href="/"
 				className="flex items-center space-x-1 text-xl font-bold"
@@ -16,14 +40,54 @@ const Header: FC = () => {
 			</a>
 
 			<ul className="hidden lg:flex items-center space-x-3">
-				<li className="navLinks border-b-4 border-schema-darkBlue">
+				<li
+					className={classNames(
+						scrolled ? "text-black" : "text-white",
+						"navLinks border-b-4 border-schema-darkBlue"
+					)}
+				>
 					Home
 				</li>
-				<li className="navLinks">Services</li>
-				<li className="navLinks">Why Us</li>
-				<li className="navLinks">Our Goals</li>
-				<li className="navLinks">Our Portfolio</li>
-				<li className="navLinks">Our Client</li>
+				<li
+					className={classNames(
+						scrolled ? "text-black" : "text-white",
+						"navLinks"
+					)}
+				>
+					Services
+				</li>
+				<li
+					className={classNames(
+						scrolled ? "text-black" : "text-white",
+						"navLinks"
+					)}
+				>
+					Why Us
+				</li>
+				<li
+					className={classNames(
+						scrolled ? "text-black" : "text-white",
+						"navLinks"
+					)}
+				>
+					Our Goals
+				</li>
+				<li
+					className={classNames(
+						scrolled ? "text-black" : "text-white",
+						"navLinks"
+					)}
+				>
+					Our Portfolio
+				</li>
+				<li
+					className={classNames(
+						scrolled ? "text-black" : "text-white",
+						"navLinks"
+					)}
+				>
+					Our Client
+				</li>
 				<li className="bg-white rounded-full px-5 py-2 hover:bg-gray-300 duration-300 ease-in-out cursor-pointer">
 					Contact Us
 				</li>
